0bb4144468f1259fa68774f85df11aba5abed5f3
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-12-09 23:17:57 +0600

    error solving

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,7 +1,10 @@
-"use server";
-import {auth} from "@clerk/nextjs/server"
+// "use server";
+// import {auth} from "@clerk/nextjs/server"
+// import { Import } from "lucide-react";
+// import { adminDB } from "@/firebase-admin";
  
-export async function createNewDocument() {
-  auth.protect();
-  const  {sessionClaims}=await auth();
-}
+// export async function createNewDocument() {
+//   auth.protect();
+//   const  {sessionClaims}=await auth();
+//   const docCollection=adminDB.collection("documents")
+// }
--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,35 +1,9 @@
-import React, { useTransition } from "react";
+"use client"
 import { Button } from "./ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-import { useRouter } from "next/router";
-import { createNewDocument } from "@/actions/actions";
-
-const NewDocumentButton = () => {
-  const [ispending,startTransition]=useTransition();
-  const router=useRouter()
-  const handleCreateDocument = () => {
-    startTransition(async()=>{
-      const {docId}=await createNewDocument()
-      router.push(`/doc/${docId}`)
-
-    })
+function NewDocumentButton() {
+  const handleNewDocument = () => {
+    
   };
-  return (
-    <div>
-      <Button onClick={handleCreateDocument} disabled={ispending} className="">
-        {ispending ? "Creating..." : "New Document"}
-      </Button>
-    </div>
-  );
-};
-
+  return <Button onClick={handleNewDocument}>Create New Document</Button>;
+}
 export default NewDocumentButton;
