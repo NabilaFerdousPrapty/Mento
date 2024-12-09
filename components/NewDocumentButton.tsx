dfd391da3ff9c5515a25ecbdb4d55c80638132dc
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-12-10 00:13:36 +0600

    issues solved

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,7 +1,14 @@
 "use client"
+import { useTransition } from "react";
 import { Button } from "./ui/button";
+import { createNewDocument } from "@/actions/actions";
 function NewDocumentButton() {
+  const [isPending, startTransition] = useTransition();
   const handleNewDocument = () => {
+    startTransition(async() => {
+      const {docId}=await createNewDocument();
+    }
+  )
     
   };
   return <Button onClick={handleNewDocument}>Create New Document</Button>;
