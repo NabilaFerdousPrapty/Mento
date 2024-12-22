b4abd278ce7531239a38d2e93119ca0c8ff4d947
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-12-22 13:59:27 +0600

    router issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-full h-screen flex-col flex justify-center items-center">
         <h1 className="text-3xl font-bold underline">
           Mento a all new AI productivity tool
         </h1>
+       
         <Button>Get Started</Button>
       </div>
     </>
--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -2,15 +2,22 @@
 import { useTransition } from "react";
 import { Button } from "./ui/button";
 import { createNewDocument } from "@/actions/actions";
+import { useRouter } from "next/navigation";
 function NewDocumentButton() {
   const [isPending, startTransition] = useTransition();
+  const Router = useRouter();
   const handleNewDocument = () => {
     startTransition(async() => {
       const {docId}=await createNewDocument();
+      Router.push(`/document/${docId}`);
     }
   )
     
   };
-  return <Button onClick={handleNewDocument}>Create New Document</Button>;
+  return <Button className="bg-gray-800 text-white hover:bg-black border-2 border-white" onClick={handleNewDocument}>
+    {
+      isPending ? "Creating ..." : "New Document"
+    }
+  </Button>;
 }
 export default NewDocumentButton;
